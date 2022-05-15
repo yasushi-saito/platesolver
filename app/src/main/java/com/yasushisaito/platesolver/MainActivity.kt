d1ed07845687546de42a0cf07a7ff12270ff3761
Yaz Saito <yasushi.saito@gmail.com>
2022-05-15 03:19:49 -0700

    Added astap_cli.

--- app/src/main/java/com/yasushisaito/platesolver/MainActivity.kt
+++ app/src/main/java/com/yasushisaito/platesolver/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("FILESDIR: ${filesDir}")
     }
 
     private fun newLauncher(cb: (Intent)->Unit) : ActivityResultLauncher<Intent> {
--- setup_emulator.sh
+++ setup_emulator.sh
@@ -4,3 +4,6 @@ if [ ! -f /tmp/h17.zip ]; then
     zip -r /tmp/h17.zip h17*
 fi
 adb push /tmp/h17.zip /sdcard/Download/
+adb push /home/saito/src/astap_android/astap.fpc/command-line_version/astap_cli /sdcard/Download/
+adb push /home/saito/src/astap_android/astap.fpc/command-line_version/astap_cli /storage/emulated/0/Android/data/com.yasushisaito.platesolver/files/
+adb shell chmod 755 /storage/emulated/0/Android/data/com.yasushisaito.platesolver/files/astap_cli
